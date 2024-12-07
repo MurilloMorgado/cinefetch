30611670e7efadc89c468036bd9cd21b0e0a5b00
MurilloMorgado <mumorgado@hotmail.com>
2024-12-07 18:00:11 -0300

    first commit

---
+++ src/main/java/br/com/murillo/cinefetch/CinefetchApplication.java
@@ -0,0 +1,13 @@
+package br.com.murillo.cinefetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/murillo/cinefetch/CinefetchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.murillo.cinefetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
