bb9ebfd47a4b0ede8f2f26b2bc08da831d475692
MurilloMorgado <mumorgado@hotmail.com>
2024-12-15 13:25:57 -0300

    Implementando a configuracao do webclient

--- src/main/java/br/com/murillo/cinefetch/framework/adapters/MovieServiceAdapter.java
+++ src/main/java/br/com/murillo/cinefetch/framework/adapters/MovieServiceAdapter.java
@@ -1,17 +1,34 @@
 package br.com.murillo.cinefetch.framework.adapters;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import br.com.murillo.cinefetch.application.output.MovieOutputPort;
 import br.com.murillo.cinefetch.domain.models.Movie;
 
-@Component
+@Service
 public class MovieServiceAdapter implements MovieOutputPort {
+
+  private static final String API_KEY = "d8c1a254";
+  private static final String URL_API = "www.omdbapi.com";
+  private final WebClient.Builder webClientBuilder;
+
+  public MovieServiceAdapter(WebClient.Builder webClientBuilder) {
+    this.webClientBuilder = webClientBuilder;
+  }
 
   @Override
   public Movie findMovieByName(String movieName) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'findMovieByName'");
+    WebClient webClient = webClientBuilder.baseUrl(URL_API).build();
+
+    return webClient.get()
+        .uri(uriBuilder -> uriBuilder
+            .queryParam("t", movieName)
+            .queryParam("apikey", API_KEY)
+            .build())
+        .retrieve()
+        .bodyToMono(Movie.class)
+        .block();
   }
-  
+
 }
