866823dd8a4e0bb4832680158eec301a12cc2e24
MurilloMorgado <mumorgado@hotmail.com>
2024-12-15 13:25:39 -0300

    implementando o service

--- src/main/java/br/com/murillo/cinefetch/application/service/MovieService.java
+++ src/main/java/br/com/murillo/cinefetch/application/service/MovieService.java
@@ -4,14 +4,19 @@ import org.springframework.stereotype.Service;
 
 import br.com.murillo.cinefetch.application.input.MovieInputPort;
 import br.com.murillo.cinefetch.domain.models.Movie;
+import br.com.murillo.cinefetch.framework.adapters.MovieServiceAdapter;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class MovieService implements MovieInputPort {
+
+  private final MovieServiceAdapter movieServiceAdapter;
 
   @Override
   public Movie findMovieByName(String movieName) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'findMovieByName'");
+    Movie movie = movieServiceAdapter.findMovieByName(movieName);
+    return movie;
   }
-  
+
 }
